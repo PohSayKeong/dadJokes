052f2cb28ce7c2423a2a912ab7fbd676f0d1d45a
PohSayKeong <poh.saykeong@dhs.sg>
2016-07-29 17:15:19 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+$(function(){
+
+	$("#getJoke").click(
+		function(){
+			$.get("/jokes",function(data){
+
+				$("#setup").html(data.setup);
+				$("#punchline").html(data.punchline);
+			},"json")
+		}
+	);
+});
+
